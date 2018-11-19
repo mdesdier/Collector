963d3d3866ed07040b4b59c676e04f7a5f11b140
marc <MDESDIER@HOTMAIL.COM>
2018-11-18 23:03:52 -0800

    get image from lib, save it to core data.  fill table vc with items from core data.

--- Collector/AddItemViewController.swift
+++ Collector/AddItemViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {  //added 'follow uiImagePickerController for delegate'
-
+    
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var itemImageView: UIImageView!
     
@@ -17,7 +17,7 @@ class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         imagePicker.delegate = self //delegate looking for class that can handle its situations.
         
@@ -25,6 +25,9 @@ class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     
     @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera //select camera
+        present(imagePicker, animated: true, completion: nil) //present image picker view controller we created to go to photo lib.  completion can be nil or function to call upon completion.
+        
     }
     @IBAction func photosTapped(_ sender: Any) {
         imagePicker.sourceType = .photoLibrary //select photo lib here, not camera
@@ -38,13 +41,28 @@ class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, 
         //get selected image from returned info dictionary
         
         if let chosenImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-                itemImageView.image = chosenImage
+            itemImageView.image = chosenImage
         }
         //need to dismiss image picker now that we have selected one
         imagePicker.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func AddTapped(_ sender: Any) {
+        if let myContext = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let item = Item(entity: Item.entity() , insertInto: myContext)
+          //  let item = Item(context: myContext)
+            
+            item.title = titleTextField.text  //copy title to core data item.
+            if let image = itemImageView.image {
+                if let imageData = image.pngData() {
+                    item.image = imageData
+                }
+            }
+            
+            try? myContext.save()   //save to core data
+            navigationController?.popViewController(animated: true)
+            
+        }
     }
     
 }
---
+++ Collector/CollectorTableViewController.swift
@@ -0,0 +1,100 @@
+//
+//  CollectorTableViewController.swift
+//  Collector
+//
+//  Created by tester on 11/18/18.
+//  Copyright © 2018 Tepo Labs. All rights reserved.
+//
+
+import UIKit
+
+class CollectorTableViewController: UITableViewController {
+
+    var items : [Item] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        getItems()
+    }
+    
+    func getItems() {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let coreDataStuff = try? context.fetch(Item.fetchRequest()) as? [Item] {
+                if let coreDataItems = coreDataStuff {
+                    items = coreDataItems
+                    tableView.reloadData()
+                    
+                }
+            }
+        }
+    }
+   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //  return the number of rows
+        return items.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        //copy-paste reuseIdentifier to storyboard table view cell identifier field
+        // here we work on / configure the cell...
+
+        let item = items[indexPath.row] // grab the item for this row from items array we got from core data
+        cell.textLabel?.text = item.title
+        
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+  
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
